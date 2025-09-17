8d473856b144ca154b5259625340518dac31afc3
Marshall Chan <yahoohung@gmail.com>
2025-09-18 01:05:06 +0800

    quick test

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -5,15 +5,6 @@ import { resolve } from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    // Ensure only one React copy is used when importing local sources
-    dedupe: ['react', 'react-dom', 'react/jsx-runtime'],
-    alias: {
-      react: resolve(__dirname, 'node_modules/react'),
-      'react-dom': resolve(__dirname, 'node_modules/react-dom'),
-      'react/jsx-runtime': resolve(__dirname, 'node_modules/react/jsx-runtime.js'),
-    }
-  },
   server: {
     fs: {
       // allow importing local source files from this monorepo (../../../src)
